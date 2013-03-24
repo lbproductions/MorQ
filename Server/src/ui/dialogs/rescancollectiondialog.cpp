13b6fd31db92b0dd24102a7dcc46aa29efa1cde1
Niclas <niclasraabe@niclass-macbook-pro.fritz.box>
2013-03-24 20:20:56 +0100

    Searching for links at scraping

--- Server/src/ui/dialogs/rescancollectiondialog.cpp
+++ Server/src/ui/dialogs/rescancollectiondialog.cpp
@@ -175,6 +175,7 @@ void RescanCollectionDialog::saveTvdbResultAndContinueToNextSeries()
                          .arg(series->title())
                          .arg(series->tvdbId()));
 
+    searchDownlaodsAtSerienjunkies();
 
     QPersistence::update(m_currentSeries);
     m_scrapedSeries.append(m_currentSeries);
@@ -217,6 +218,29 @@ void RescanCollectionDialog::cleanupTvdbResultsPage()
     m_provider = nullptr;
     m_seriesDao = nullptr;
     m_seriesListModel = nullptr;
+}
+
+void RescanCollectionDialog::searchDownlaodsAtSerienjunkies()
+{
+    DownloadProviderPlugin *plugin = Controller::plugins()->downloadProviderPluginByName("serienjunkies.org");
+
+    plugin->searchSeries(m_currentSeries->title());
+
+    connect(plugin, &DownloadProviderPlugin::foundSeries, this, &RescanCollectionDialog::downloadsFoundAtSerienjunkies);
+}
+
+void RescanCollectionDialog::downloadsFoundAtSerienjunkies(QList<DownloadProviderPlugin::SeriesData> series)
+{
+
+    //TODO: Find a better way to get the right serie (with not the same name)
+    Series* serie = Controller::seriesDao()->byTitle(series.first().title);
+
+    if(serie != 0 && serie->serienJunkiesUrl().toString() == ""){
+       serie->setSerienJunkiesUrl(series.first().url);
+       Controller::plugins()->downloadProviderPluginByName("serienjunkies.org")->findMissingEpisodes(serie);
+       ui->textEdit->append("Serienjunkies-Url for " + series.first().title + " found: " + series.first().url.toString());
+       QPersistence::update(serie);
+    }
 }
 
 void RescanCollectionDialog::scrape()
